dd19061b6978ef9492212b53f45bf8d77e1b6fe4
MinhLuanLu <141078665+MinhLuanLu@users.noreply.github.com>
2025-02-28 10:34:40 +0100

    Update color-log.js

--- color-log.js
+++ color-log.js
@@ -2,19 +2,19 @@ import clc from 'cli-color'
 
 const log = {
     log: (value) => {
-        console.info(clc.green(value));
+        console.info(clc.green(`[LOG] ${value}`));
     },
     info: (value) => {
-        console.info(clc.cyan(value));
+        console.info(clc.cyan(`[INFO] ${value}`));
     },
     debug: (value) => {
-        console.debug(clc.magenta(value)); 
+        console.debug(clc.magenta(`[DEBUG] ${value}`)); 
     },
     warn: (value) => {
-        console.warn(clc.yellow(value)); 
+        console.warn(clc.yellow(`WARN ${value}`)); 
     },
     error: (value) => {
-        console.error(clc.red(value));
+        console.error(clc.red(`[ERROR] ${value}`));
     }
 };
 
