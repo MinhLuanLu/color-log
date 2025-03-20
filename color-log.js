1477a23b3df26a3851a313d145f8e3ceb031f6b6
MinhLuanLu <141078665+MinhLuanLu@users.noreply.github.com>
2025-03-20 15:28:30 +0100

    Update color-log.js

--- color-log.js
+++ color-log.js
@@ -1,20 +1,78 @@
-import clc from 'cli-color'
+import clc from 'cli-color';
+
+const formatValue = (key, value) => {
+
+    const whiteEqual = clc.white('=');
+    const whiteColon = clc.white(':');
+    if (key === 'data') {
+        // Format the "data" object in white
+        return `${clc.white(key)} ${whiteEqual} ${JSON.stringify(value)}`;
+    } else if (typeof value === 'object') {
+        // Format other objects normally
+        return `${clc.white(key)}${whiteColon} ${JSON.stringify(value)}`;
+    }
+    return `${clc.white(key)}${whiteColon} ${value}`;
+};
 
 const log = {
-    log: (value) => {
-        console.log(clc.green(`[LOG] ${value}`));
+    log: (obj) => {
+        if (typeof obj === 'object' && obj !== null) {
+            const formattedMessage = Object.entries(obj)
+                .map(([key, value]) => formatValue(key, value))
+                .join(', ');
+
+            console.log(clc.green(`[LOG] ${formattedMessage}`));
+        } else {
+            console.log(clc.green(`[LOG] ${obj}`));
+        }
     },
-    info: (value) => {
-        console.info(clc.cyan(`[INFO] ${value}`));
+
+    info: (obj) => {
+        if (typeof obj === 'object' && obj !== null) {
+            const formattedMessage = Object.entries(obj)
+                .map(([key, value]) => formatValue(key, value))
+                .join(', ');
+
+            console.log(clc.cyan(`[INFO] ${formattedMessage}`));
+        } else {
+            console.log(clc.cyan(`[INFO] ${obj}`));
+        }
     },
-    debug: (value) => {
-        console.debug(clc.magenta(`[DEBUG] ${value}`)); 
+
+    debug: (obj) => {
+        if (typeof obj === 'object' && obj !== null) {
+            const formattedMessage = Object.entries(obj)
+                .map(([key, value]) => formatValue(key, value))
+                .join(', ');
+
+            console.log(clc.magenta(`[DEBUG] ${formattedMessage}`));
+        } else {
+            console.log(clc.magenta(`[DEBUG] ${obj}`));
+        }
     },
-    warn: (value) => {
-        console.warn(clc.yellow(`[WARN] ${value}`)); 
+
+    warn: (obj) => {
+        if (typeof obj === 'object' && obj !== null) {
+            const formattedMessage = Object.entries(obj)
+                .map(([key, value]) => formatValue(key, value))
+                .join(', ');
+
+            console.log(clc.yellow(`[WARN] ${formattedMessage}`));
+        } else {
+            console.log(clc.yellow(`[WARN] ${obj}`));
+        }
     },
-    error: (value) => {
-        console.error(clc.red(`[ERROR] ${value}`));
+
+    err: (obj) => {
+        if (typeof obj === 'object' && obj !== null) {
+            const formattedMessage = Object.entries(obj)
+                .map(([key, value]) => formatValue(key, value))
+                .join(', ');
+
+            console.log(clc.red(`[ERROR] ${formattedMessage}`));
+        } else {
+            console.log(clc.red(`[ERROR] ${obj}`));
+        }
     }
 };
 
